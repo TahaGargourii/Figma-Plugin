82a662a862f1772013924b1dde7d98e1cd266704
Taha Gargouri <taha.gargouri@itcexpertise.com>
2024-02-06 15:19:00 +0100

    first commit

---
+++ code.ts
@@ -0,0 +1,13 @@
+// This is the main code for the plugin that will run within Figma.
+// It will open the UI defined in 'ui.html'.
+figma.showUI(__html__, { width: 240, height: 320 });
+
+// Handle messages received from the UI
+figma.ui.onmessage = msg => {
+  if (msg.type === 'save-api-key') {
+    // Save the API key using Figma client storage API
+    figma.clientStorage.setAsync('apiKey', msg.apiKey).then(() => {
+      figma.closePlugin(`API Key saved: ${msg.apiKey}`);
+    });
+  }
+};
